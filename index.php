aaa84b2290ef0167bd1157d4662d178cfc8457b2
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2017-09-10 12:06:10 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Web App</title>
+        <?php echo 'Hello' ?>
+    </head>
+    
+    <body>
+        <div id = "main-title">
+            <center><h1>Web Application</h1></center>
+        </div>
+        
+        <div id = "main-content">
+            <center>
+                <form action = "web_app_server.php" method="get">
+                    <h2>Login</h2>
+                    <table border="0">
+                        <tr>
+                            <td><label for="username">Username</label></td>
+                            <td><input type="text" name="username" id="username"></td>
+                        </tr>
+                        
+                        <tr>
+                            <td><label for="password">Password</label></td>
+                            <td><input name="password" type="password" id="password"></td>
+                        </tr>
+                        
+                        <tr>
+                            <td><input type="submit" value="Submit"/>
+                            <td><input type="reset" value="Reset"/>
+                        </tr>
+                    </table>
+                </form>
+            </center>
+        </div>
+    </body>
+</html>
