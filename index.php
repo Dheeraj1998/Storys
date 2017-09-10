c9eba052d62d687c6680841ffbaab19028a37cec
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2017-09-10 14:15:52 +0530

    First implementation of UI kit :coffee:

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <html>
     <head>
+        <link src = "paperkit-2/assets/css/paper-kit.css" type="text/css" rel="stylesheet">
         <title>Web App</title>
         <?php echo 'Hey there this is Shantanu!!!' ?>
     </head>
-    
+
     <body>
         <div id = "main-title">
             <center><h1>Web Application</h1></center>
         </div>
-        
+
         <div id = "main-content">
             <center>
                 <form action = "web_app_server.php" method="get">
@@ -18,14 +19,14 @@
                             <td><label for="username">Username</label></td>
                             <td><input type="text" name="username" id="username"></td>
                         </tr>
-                        
+
                         <tr>
                             <td><label for="password">Password</label></td>
                             <td><input name="password" type="password" id="password"></td>
                         </tr>
-                        
+
                         <tr>
-                            <td><input type="submit" value="Submit"/>
+                            <td><input type="submit" value="Submit" class="btn btn-success"/>
                             <td><input type="reset" value="Reset"/>
                         </tr>
                     </table>
