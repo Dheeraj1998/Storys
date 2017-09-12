44220c8253ff90aa4f88bf9c511280878b021f0d
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2017-09-12 12:05:59 +0530

    Link and code re-format.

--- index.php
+++ index.php
@@ -1,73 +1,76 @@
+<!DOCTYPE html>
+
 <html>
-
 <head>
-    <link src="paperkit2/assets/css/paper-kit.css" type="text/css" rel="stylesheet">
-    <meta charset="utf-8"/>
-    <link rel="stylesheet" href="styles.css" type="text/css">
-    <!--    <link href="paperkit2/assets/css/bootstrap.min.css" rel="stylesheet"/>-->
-    <link href="paperkit2/assets/css/paper-kit.css" rel="stylesheet"/>
-    <link href="paperkit2/assets/css/demo.css" rel="stylesheet"/>
+	<link rel="stylesheet" type="text/css">
+	<meta charset="utf-8">
+	<link href="styles.css" rel="stylesheet" type="text/css">
+	<!--    <link href="paperkit2/assets/css/bootstrap.min.css" rel="stylesheet"/>-->
+	<link href="paperkit2/assets/css/paper-kit.css" rel="stylesheet">
+	<link href="paperkit2/assets/css/demo.css" rel="stylesheet">
+	<!--     Fonts and icons     -->
+	<link href="paperkit2/assets/img/apple-icon.png" rel="apple-touch-icon" sizes=
+	"76x76">
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700'
+	rel='stylesheet' type='text/css'>
+	<link href=
+	"http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"
+	rel="stylesheet">
+	<link href="paperkit2/assets/img/favicon.ico" rel="icon" type="image/png">
+	<link href="paperkit2/assets/css/nucleo-icons.css" rel="stylesheet">
+	<script src=
+	"https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js">
+	</script>
 
-    <!--     Fonts and icons     -->
-    <link rel="apple-touch-icon" sizes="76x76" href="paperkit2/assets/img/apple-icon.png">
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="icon" type="image/png" href="paperkit2/assets/img/favicon.ico">
-    <link href="paperkit2/assets/css/nucleo-icons.css" rel="stylesheet">
-
-    <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
-    <title>Storys</title>
+	<title>Storys</title>
 </head>
 
 <body>
-
-<nav>
-    <h5 class="developed">Developed by
-        <img src="paperkit2/assets/img/faces/dheeraj.jpg">
-        <span class="blank-space"></span>Dheeraj &
-        <img src="paperkit2/assets/img/faces/shantanu.jpg">
-        <span class="blank-space"> </span>Shantanu</h5>
-
-    <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-            <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank">
-                <i class="fa fa-twitter"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank">
-                <i class="fa fa-facebook-square"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
-                <i class="fa fa-instagram"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" rel="tooltip" title="Star on GitHub" data-placement="bottom" href="https://www.github.com/CreativeTimOfficial/paper-kit" target="_blank">
-                <i class="fa fa-github"></i>
-            </a>
-        </li>
-    </ul>
-
-</nav>
+	<nav>
+		<h5 class="developed">Developed by <a href=
+		"https://www.github.com/Dheeraj1998" target="_blank"><img src=
+		"paperkit2/assets/img/icon-github.png"> <img class="github-profile-pic" src=
+		"paperkit2/assets/img/faces/dheeraj.jpg"></a> <span class=
+		"blank-space"></span>Dheeraj & <a href="https://github.com/shantanu0323"
+		target="_blank"><img src="paperkit2/assets/img/icon-github.png"> <img class=
+		"github-profile-pic" src="paperkit2/assets/img/faces/shantanu.jpg"></a>
+		<span class="blank-space"></span>Shantanu</h5>
 
 
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+				<a class="nav-link" data-placement="bottom" href=
+				"https://twitter.com/shantanu0323" rel="tooltip" target="_blank" title=
+				"Follow us on Twitter"><i class="fa fa-twitter"></i></a>
+			</li>
 
 
+			<li class="nav-item">
+				<a class="nav-link" data-placement="bottom" href=
+				"https://www.facebook.com/shantanu.pramanik1" rel="tooltip" target="_blank"
+				title="Like us on Facebook"><i class="fa fa-facebook-square"></i></a>
+			</li>
 
 
+			<li class="nav-item">
+				<a class="nav-link" data-placement="bottom" href=
+				"https://www.instagram.com/shantanu0323" rel="tooltip" target="_blank"
+				title="Follow us on Instagram"><i class="fa fa-instagram"></i></a>
+			</li>
+		</ul>
+	</nav>
 
 
+	<div class="outer-container">
+		<h1 class="main-title">Storys</h1>
 
-<div class="outer-container">
-    <h1 class="main-title">Storys</h1>
-    <h3 class="tagline">Write | Endorse | Like | Share | Chat</h3>
-    <div class="moving-clouds" style="background-image: url('paperkit2/assets/img/clouds.png'); ">
 
-    </div>
-</div>
+		<h3 class="tagline">Write | Endorse | Like | Share | Chat</h3>
 
+
+		<div class="moving-clouds" style=
+		"background-image: url('paperkit2/assets/img/clouds.png');">
+		</div>
+	</div>
 </body>
 </html>
