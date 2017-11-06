bb0cb022f25280e98c4d98eaa5b40e1163da407c
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2017-11-06 15:46:09 +0530

    Integrated Firebase.

--- analysis.php
+++ analysis.php
@@ -5,10 +5,26 @@
   <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
   <script src="https://code.highcharts.com/highcharts.js"></script>
   <script src="https://code.highcharts.com/modules/exporting.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/4.6.1/firebase.js"></script>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDxv4bVHpxdauO3Huermqgoy86jPun8BUQ&callback=initMap" type="text/javascript"></script>
+  <script>
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyC5jz22XXOsoMhBQC5m8Is3ocn7rctWn5s",
+      authDomain: "storys-analytics.firebaseapp.com",
+      databaseURL: "https://storys-analytics.firebaseio.com",
+      projectId: "storys-analytics",
+      storageBucket: "storys-analytics.appspot.com",
+      messagingSenderId: "497967477864"
+    };
 
+    firebase.initializeApp(config);
+    var database = firebase.database();
+    var locationRef = firebase.database().ref('user_location');
+  </script>
 </head>
 
-<body>
+<body onload="initMap()" >
   <?php
   $servername = "localhost";
   $db_username = "root";
@@ -52,54 +68,107 @@
   <div id="container" style="min-width: 600px; height: 600px; max-width: 600px; margin: 0 auto"></div>
 
   <script>
-  // Build the chart
-Highcharts.chart('container', {
-  chart: {
-      plotBackgroundColor: null,
-      plotBorderWidth: null,
-      plotShadow: false,
-      type: 'pie'
-  },
-  title: {
-      text: 'Category Wise Popularity'
-  },
-  tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-  },
-  plotOptions: {
-      pie: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          dataLabels: {
-              enabled: false
-          },
-          showInLegend: true
-      }
-  },
-  series: [{
-      name: 'Categories',
-      colorByPoint: true,
-      data: [{
-          name: '#Technology',
-          y: <?php echo $num_tech ?>
-      }, {
-          name: '#Lifestyle',
-          y: <?php echo $num_life ?>,
-          sliced: true,
-          selected: true
-      }, {
-          name: '#Music',
-          y: <?php echo $num_music ?>
-      }, {
-          name: '#Literature',
-          y: <?php echo $num_lit ?>
-      }, {
-          name: '#Photography',
-          y: <?php echo $num_photo ?>
+    // Build the chart
+    Highcharts.chart('container', {
+      chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: 'pie'
+      },
+      title: {
+          text: 'Category Wise Popularity'
+      },
+      tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+          pie: {
+              allowPointSelect: true,
+              cursor: 'pointer',
+              dataLabels: {
+                  enabled: false
+              },
+              showInLegend: true
+          }
+      },
+      series: [{
+          name: 'Categories',
+          colorByPoint: true,
+          data: [{
+              name: '#Technology',
+              y: <?php echo $num_tech ?>
+          }, {
+              name: '#Lifestyle',
+              y: <?php echo $num_life ?>,
+              sliced: true,
+              selected: true
+          }, {
+              name: '#Music',
+              y: <?php echo $num_music ?>
+          }, {
+              name: '#Literature',
+              y: <?php echo $num_lit ?>
+          }, {
+              name: '#Photography',
+              y: <?php echo $num_photo ?>
+          }]
       }]
-  }]
-});
+    });
   </script>
+
+<script type="text/javascript">
+  var geocoder;
+  var map;
+
+  function initMap() {
+    geocoder = new google.maps.Geocoder();
+    var myOptions = {
+      zoom: 2,
+      center: new google.maps.LatLng(0, 0),
+      mapTypeControl: true,
+      mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+      navigationControl: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+    locationRef.on('value', function(snapshot){
+      snapshot.forEach(function(child){
+        var address = child.val();
+
+        geocoder.geocode( { 'address': address}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
+
+              var infowindow = new google.maps.InfoWindow({
+                  content: '<b>' + child.key + '</b> - ' + address,
+                  size: new google.maps.Size(150,50)
+              });
+
+              var marker = new google.maps.Marker({
+                  position: results[0].geometry.location,
+                  map: map,
+                  title:address
+              });
+              google.maps.event.addListener(marker, 'click', function() {
+                  infowindow.open(map,marker);
+              });
+
+            } else {
+              alert("No results found");
+            }
+          } else {
+            alert("Geocode was not successful for the following reason: " + status);
+          }
+        });
+      });
+    });
+  }
+</script>
+
+<div id="map_canvas" style="width:700px; height:500px">
 
 </body>
 </html>
--- register.php
+++ register.php
@@ -7,17 +7,26 @@
     <link href="assets_folder/assets/css/main.css" rel="stylesheet">
     <link href="assets_folder/assets/css/demo.css" rel="stylesheet">
     <!--     Fonts and icons     -->
-    <link href="assets_folder/assets/img/apple-icon.png" rel="apple-touch-icon" sizes=
-    "76x76">
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700'
-          rel='stylesheet' type='text/css'>
-    <link href=
-          "http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"
-          rel="stylesheet">
+    <link href="assets_folder/assets/img/apple-icon.png" rel="apple-touch-icon" sizes="76x76">
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     <link href="assets_folder/assets/img/favicon.ico" rel="icon" type="image/png">
     <link href="assets_folder/assets/css/nucleo-icons.css" rel="stylesheet">
-    <script src=
-            "https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js">
+    <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/4.6.1/firebase.js"></script>
+    <script>
+      // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyC5jz22XXOsoMhBQC5m8Is3ocn7rctWn5s",
+        authDomain: "storys-analytics.firebaseapp.com",
+        databaseURL: "https://storys-analytics.firebaseio.com",
+        projectId: "storys-analytics",
+        storageBucket: "storys-analytics.appspot.com",
+        messagingSenderId: "497967477864"
+      };
+      firebase.initializeApp(config);
+
+      var database = firebase.database();
     </script>
     <title>Register</title>
 </head>
@@ -68,6 +77,10 @@
                 <td><input class="name" type="text" name="name" placeholder="Name"></td>
             </tr>
             <tr>
+                <td>Location</td>
+                <td><input class="name" type="text" name="location" placeholder="Location"></td>
+            </tr>
+            <tr>
                 <td>Email</td>
                 <td><input class="email" type="email" name="email" placeholder="Email"></td>
             </tr>
@@ -88,21 +101,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = hash('md4', $_POST['password']);
     $name = $_POST['name'];
+    $location = $_POST['location'];
     $email = $_POST['email'];
 
     $servername = "localhost";
     $db_username = "root";
     $db_password = file_get_contents('password.txt');
     $db_name = "Storys";
+    $firebase_database = "user_location";
 
     //Create connection
     $conn = new mysqli("$servername", $db_username, $db_password, $db_name);
+    //$sql = "INSERT INTO UserAccounts (Username, Password, Name, Email) VALUES ('" . $username . "', '" . $password . "', '" . $name . "', '" . $email . "');";
 
-    $sql = "INSERT INTO UserAccounts (Username, Password, Name, Email) VALUES ('" . $username . "', '" . $password . "', '" . $name . "', '" . $email . "');";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-        header("Location: login.php");
+  if (/*$conn->query($sql) === TRUE */ TRUE) {
+      echo "<script>
+              database.ref('" . $firebase_database . "').set({
+                '" . $username . "': '" . $location . "'
+              });
+              alert('The registration has been done!');
+            </script>";
         die();
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
